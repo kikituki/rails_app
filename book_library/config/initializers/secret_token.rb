55818e935a4f900892007cb0a13aa911c712b106
vagrant <vagrant@localhost.localdomain>
2014-04-03 09:07:32 +0000

    first commit

---
+++ book_library/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookLibrary::Application.config.secret_key_base = '655be538e4696c7573f9daacd2452d50516cdcdbcf533022116906cae4f94ee39dd45732cef04a83e3612add20b349e78dec7c2a177aa44e28e116a120b719eb'
