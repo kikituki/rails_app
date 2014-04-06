e075cfc2c3d8f31dbfa637b0ccca9e14c4427c1a
vagrant <vagrant@localhost.localdomain>
2014-04-06 08:44:45 +0000

    mod little

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -16,22 +16,44 @@ describe "StaticPages" do
   		visit '/static_pages/home'
   		expect(page).to have_content('Sample App')
   	end
+
+  	#タイトルのテスト
+  	it "should have the right title'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+  	end
+
   end
 
  describe "Help page" do
   	#画面には’this is Help Page’と表示されるはず
   	it "should have the content 'this is Help Page'" do
   		visit '/static_pages/help'
-  		expect(page).to have_content('this is Help Page')
+  		expect(page).to have_content('Help')
   	end
+
+  	#タイトルのテスト
+  	it "should have the right title'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+  	end
+
+
   end
 
- describe "About page" do
+ describe "About Page" do
   	#画面には’About Page’と表示されるはず
-  	it "should have the content 'About Page'" do
+  	it "should have the content 'About Us'" do
   		visit '/static_pages/about'
-  		expect(page).to have_content('About Page')
+  		expect(page).to have_content('About Us')
   	end
-  end
 
+  	#タイトルのテスト
+  	it "should have the right title'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+  	end
+
+
+  end
 end
